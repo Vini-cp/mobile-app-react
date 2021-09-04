e29878f3012b9b3485d66870b01ca4d999dd10d4
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-09-04 18:23:49 -0300

    Create a components directory with a search file

---
+++ Components/Search.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StyleSheet, View, Button, TextInput } from 'react-native'
+
+class Search extends React.Component {
+  render () {
+    return (
+      <View style={ styles.main_container }>
+        <TextInput style={ styles.textinput } placeholder="Titre du filme"/>
+        <Button title="Rechercher" onPress={() => {}}/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
+})
+
+export default Search
