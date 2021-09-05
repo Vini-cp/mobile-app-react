3ab0e6b11e7becbf6548667444cd0ea5dd8099a4
Vinícius Cordeiro Pereira <62481046+Vini-cp@users.noreply.github.com>
2021-09-04 22:51:19 -0300

    Add an API call

---
+++ API/TMDBApi.js
@@ -0,0 +1,8 @@
+const API_TOKEN = "API_KEY";
+
+export function getFilmsFromApiWithSearchedText (text) {
+  const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error))
+}
--- Components/Search.js
+++ Components/Search.js
@@ -1,20 +1,40 @@
 import React from 'react'
-import { StyleSheet, View, Button, TextInput, FlatList, Text } from 'react-native'
-
-import films from '../Helpers/filmsData'
+import { StyleSheet, View, Button, TextInput, FlatList } from 'react-native'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 import FilmItem from './FilmItem'
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props)
+    this.searchedText = ""
+    this.state = { films: [] }
+  }
+
+  _loadFilms() {
+    if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+      getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+          this.setState({ films: data.results })
+      })
+    }
+  }
+
+  _searchTextInputChanged(text) {
+    this.searchedText = text
+  }
+
   render () {
     return (
       <View style={ styles.main_container }>
-        <TextInput style={ styles.textinput } placeholder="Titre du filme"/>
-        <Button title="Rechercher" onPress={() => {}}/>
+        <TextInput
+          style={ styles.textinput }
+          placeholder="Titre du filme"
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+        />
+        <Button title="Rechercher" onPress={() => this._loadFilms()}/>
         <FlatList
-          data={films}
+          data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({item}) => <FilmItem film={item}/>}
-          // <Text>{item.title}</Text>}
         />
       </View>
     )
@@ -29,6 +49,8 @@ const styles = StyleSheet.create({
   textinput: {
     marginLeft: 5,
     marginRight: 5,
+    marginTop: 40,
+    marginBottom: 10,
     height: 50,
     borderColor: '#000000',
     borderWidth: 1,
